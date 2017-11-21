ae53062d0cdf1de91dcf2fbc5551d84dc56f8aa7
Mustafa991 <mustafa.abbas.te15c@gmail.com>
2017-11-21 10:18:05 +0100

    Skapade getters

--- src/Person.java
+++ src/Person.java
@@ -3,5 +3,13 @@ public class Person {
 	
 	private String förNamn;
 	private String efterNamn;
+	
+	
+	public String getFörNamn() {
+		return förNamn;
+	}
+	public String getEfterNamn() {
+		return efterNamn;
+	}
 
 }
