b5c26b1b018fd8cfab36d99afd549fac46f71b2e
Mustafa991 <mustafa.abbas.te15c@gmail.com>
2017-11-21 10:37:49 +0100

    Skapade konstruktor för Person och testade min kod

--- src/Person.java
+++ src/Person.java
@@ -4,6 +4,17 @@ public class Person {
 	private String förNamn;
 	private String efterNamn;
 	
+	public Person(String namn){
+		
+		int index = namn.split(" ").length;
+		
+		this.förNamn = namn.split(" ")[index-1];
+		
+		this.efterNamn="";
+		
+		for(int i =0;i<index-1;i++) this.efterNamn += namn.split(" ")[i] + " ";
+	
+	}
 	
 	public String getFörNamn() {
 		return förNamn;
---
+++ src/Test.java
@@ -0,0 +1,13 @@
+
+public class Test {
+	
+	public static void main(String[] args) {
+		
+		Person p = new Person("ef1 ef2 ef33 e för2");
+		
+		System.out.println(p.getEfterNamn());
+		
+		System.out.println(p.getFörNamn());
+	}
+
+}
